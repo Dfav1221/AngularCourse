34551d1bb0a38c4796654627f9cfe218eab85368
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-03 14:31:19 +0200

    Added basic logic to components added some dummy data

--- first-app/src/app/receipe/recipe-list/recipe-list.component.ts
+++ first-app/src/app/receipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.mode";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("spaghetti","italian","https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/spaghetti_bolognese_01.jpg"),
+    new Recipe("baguette","french","https://lh3.googleusercontent.com/proxy/rw-6lb-r2z0NtVnUg5Cse1kfSoKdjaoUEEeD9hAtPIphPw3UGcSCvTVnbVpumG4kaPDtC8OPFQbUMMHbrHXgkXZSJwGbUf3EIp1myZFXd3rFt8YnbrTc5OtltvCxRWn8")
+  ];
 
   constructor() { }
 
