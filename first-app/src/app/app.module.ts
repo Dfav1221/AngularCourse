8ad690253d56c31119e74fcde8da0a2a9e2ef42b
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-03 12:27:30 +0200

    base component setup

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,11 +2,29 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping/shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './receipe/recipe.component';
+import { RecipeItemComponent } from './receipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './receipe/recipe-details/recipe-details.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import {RecipeListComponent} from "./receipe/recipe-list/recipe-list.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    HeaderComponent,
+    RecipeComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShoppingComponent,
   ],
   imports: [
     BrowserModule,
