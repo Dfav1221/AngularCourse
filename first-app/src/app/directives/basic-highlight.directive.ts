f1defeacba7bce3e2b0ee03b0e88db98ad56a21b
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-05 10:34:12 +0200

    created my own directive which use renderer

--- first-app/src/app/directives/basic-highlight.directive.ts
+++ first-app/src/app/directives/basic-highlight.directive.ts
@@ -1,15 +1,33 @@
-import {Directive, ElementRef, OnInit} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from "@angular/core";
 
 
 @Directive({
   selector:'[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit{
-  constructor(private elementRef:ElementRef) {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+
+  constructor(private elRef: ElementRef,private renderer: Renderer2) {
 
   }
 
   ngOnInit(): void {
-    this.elementRef.nativeElement.style.backgroundColor = 'blue';
+
   }
+
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
+
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    //this.renderer.setStyle(this.elRef.nativeElement,'background-color','green');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    //this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    this.backgroundColor = this.defaultColor;
+  }
+
+
+
 }
