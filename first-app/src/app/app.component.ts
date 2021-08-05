82e54528451bc22c15ac711f3f2e3895b6fcfc35
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-05 13:11:24 +0200

    Dependency Injection playground

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountsService} from "./accounts.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[AccountsService]
 })
-export class AppComponent {
-  name = 'Name';
+export class AppComponent implements OnInit{
+  accounts:{name:string,status:string}[] = []
+
+  constructor(private accountsService: AccountsService) {
+  }
+
+  ngOnInit(){
+    this.accounts = this.accountsService.accounts;
+  }
+
+  onStatusChanged($event: { id: number; newStatus: string }) {
+    
+  }
+
+  onAccountAdded($event: { name: string; status: string }) {
+    
+  }
 }
