283585628fc8910bd2f7593587505e586f5ff28c
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-05 10:29:41 +0200

    created my own directive which use reference to DOM object

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
+import {BasicHighlightDirective} from "./directives/basic-highlight.directive";
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
