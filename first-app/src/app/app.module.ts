d29f4fd46464612431c15a47eb4985c4b14de10e
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-09 12:54:31 +0200

    Template Forms task

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
