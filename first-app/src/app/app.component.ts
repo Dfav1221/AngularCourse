575db40150220304bbe37770c426c2025465d6a5
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-09 15:05:02 +0200

    Task done

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  name = 'Name';
+export class AppComponent implements OnInit {
+  ProjectOptions = ['stable', 'Critical', 'Finished'];
+  projectForm: FormGroup;
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required],this.projectNameValidator),
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'status': new FormControl('stable')
+    });
+  }
+
+  projectNameValidator(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test') {
+          resolve({'projectNameInvalid': true});
+        } else {
+          return null;
+        }
+      }, 1500);
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
 }
