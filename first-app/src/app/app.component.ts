fa3f0370b276b6e3f3fcf8005a6acd903aa3a3b5
SIIPOLSKA\jhirsz <jhirsz@sii.pl>
2021-08-24 13:45:32 +0200

    Playground for http requests

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {map} from "rxjs/operators";
+import {Post} from "./post.model";
+import {PostsService} from "./posts.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  name = 'Name';
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private PostService: PostsService) {
+  }
+
+  ngOnInit() {
+    this.PostService.fetchPosts().subscribe(posts=>{
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.PostService.createAndStorePost(postData.title,postData.content)
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    this.fetchPosts();
+    console.log(this.loadedPosts);
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.PostService.DeletePosts().subscribe(data=>{
+      this.loadedPosts = [];
+    });
+  }
+
+  private fetchPosts() {
+
+    this.isFetching = true;
+    this.PostService.fetchPosts().subscribe(posts=>{
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
+  }
+
+  onDeletePost(index) {
+    const id = this.loadedPosts[index].id;
+    this.PostService.DeletePosts(id).subscribe();
+  }
 }
